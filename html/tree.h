13107155e4aeb62fc0139e1ec20ee15cc56734c9
skshin <skshin@gmail.com>
2018-09-27 10:35:05 +0900

    Fix error: 'ptrdiff_t' does not name a type (Davi de Castro Reis 2015-12-08)

--- html/tree.h
+++ html/tree.h
@@ -23,7 +23,7 @@
    
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
    
    TODO: - 'Move' members are long overdue; will hopefully be incorporated in the
            next release.
@@ -46,6 +46,7 @@
 #define tree_hh_
 
 #include <cassert>
+#include <cstddef>
 #include <memory>
 #include <stdexcept>
 #include <iterator>
